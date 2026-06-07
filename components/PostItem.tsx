4d78f70da38151619af22efce0c1c4f4ff7620eb
BumgeunSong <isp1195@gmail.com>
2026-06-07 21:59:18 +0900

    포스트 행 호버를 opacity 디밍에서 배경 하이라이트로 전환
    opacity-60 으로 행 전체를 흐리게 만들면 사용자가 겨냥 중인 항목이
    오히려 옅어져 직관에 어긋남. 작은 음수 마진과 둥근 모서리로 행을
    덮는 hover:bg-black/[0.04] 배경을 깔아 타겟을 강조하는 방향으로
    바꿈. transition 도 background-color, transform 만 명시해
    불필요한 프로퍼티 전환을 막음.

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -15,7 +15,7 @@ export function PostItem({ post }: { post: Post }) {
   return (
     <Link
       href={post.route}
-      className="flex flex-col gap-1 py-3 border-b border-gray-500/15 last:border-b-0 hover:opacity-60 active:scale-[0.99] transition-[opacity,transform] duration-150 ease-out origin-left"
+      className="flex flex-col gap-1 py-3 px-2 -mx-2 rounded-md border-b border-gray-500/15 last:border-b-0 hover:bg-black/[0.04] dark:hover:bg-white/[0.05] active:scale-[0.99] transition-[background-color,transform] duration-150 ease-out origin-left"
     >
       {date && (
         <span className="text-sm text-gray-500 tabular-nums">{date}</span>
